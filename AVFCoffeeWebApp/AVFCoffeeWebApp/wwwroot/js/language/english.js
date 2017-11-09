b71a8293e7a91658027105ac0dcbdeb85e986aef
mb992 <mb992@cornell.edu>
2017-11-09 12:40:28 -0500

    update front end

--- AVFCoffeeWebApp/AVFCoffeeWebApp/wwwroot/js/language/english.js
+++
@@ -1,6 +0,0 @@
-﻿/* home */
-englishObj = 
-{
-  "home-header": "Welcome",
-  "home-subheader": "Cost Calculator:  A Coffee Cost of Production Tool" 
-}
---
+++ AVFCoffeeWebApp/AVFCoffeeWebApp/wwwroot/pages/demo/js/demo.js
@@ -0,0 +1,13 @@
+﻿// when user clicks next link, it checks href for the next div to show 
+// and hides the others 
+
+$('.demo-screen').hide();
+$('#demo-screen1').fadeIn(1000);
+
+$('.slide').click(function () {
+    var next = $(this).attr("href");
+    console.log(next);
+    $('.demo-screen').hide();
+    $(next).fadeIn(1000)
+});
+
