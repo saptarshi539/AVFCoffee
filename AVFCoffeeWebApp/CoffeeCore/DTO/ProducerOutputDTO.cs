326d67ca7e9ecdee5ce2595ca1c51bec2a5a1843
Rishi <sm954@cornell.edu>
2017-11-15 11:13:40 -0500

    Changes names of the output model properties to "Cost" from "Costs"

--- AVFCoffeeWebApp/CoffeeCore/DTO/ProducerOutputDTO.cs
+++ AVFCoffeeWebApp/CoffeeCore/DTO/ProducerOutputDTO.cs
@@ -7,10 +7,10 @@ namespace CoffeeCore.DTO
     public class ProducerOutputEnglishDTO
     {
         public string userID { get; set; }
-        public Double variableCostsUSPound { get; set; }
-        public Double fixedCostsUSPound { get; set; }
+        public Double variableCostUSPound { get; set; }
+        public Double fixedCostUSPound { get; set; }
         public Double totalCostAndDeprUSPound { get; set; }
-        public Double totalCostsUSPound { get; set; } 
+        public Double totalCostUSPound { get; set; } 
         public Double breakEvenCostUSPound { get; set; }
     }
 }
--- AVFCoffeeWebApp/CoffeeCore/DTO/ProducerOutputSpanishDTO.cs
+++ AVFCoffeeWebApp/CoffeeCore/DTO/ProducerOutputSpanishDTO.cs
@@ -7,8 +7,8 @@ namespace CoffeeCore.DTO
     public class ProducerOutputSpanishDTO
     {
         public string userID { get; set; }
-        public Double variableCostsUSHect { get; set; }
-        public Double variableCostsSolesHect { get; set; }
+        public Double variableCostUSHect { get; set; }
+        public Double variableCostSolesHect { get; set; }
         public Double totalCostUSHect { get; set; }
         public Double totalCostSolesHect { get; set; }
         public Double breakEvenCostUSPound { get; set; }
