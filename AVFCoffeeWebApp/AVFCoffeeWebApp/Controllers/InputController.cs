c378c1321c55a4dbe78f0b81d9f0002fd96e4819
mb992 <mb992@cornell.edu>
2017-11-20 12:54:23 -0500

    Move api url to configuration file

--- AVFCoffeeWebApp/AVFCoffeeWebApp/Controllers/InputController.cs
+++ AVFCoffeeWebApp/AVFCoffeeWebApp/Controllers/InputController.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
 
 namespace AVFCoffeeWebApp.Controllers
 {
     public class InputController : Controller
     {
+        private readonly IConfiguration _iconfiguration;
+        private readonly IHostingEnvironment _env;
+
+        public InputController(IConfiguration iconfiguration, IHostingEnvironment env)
+        {
+            _iconfiguration = iconfiguration;
+            _env = env;
+        }
+
         public IActionResult Index()
         {
+            ViewData["apiURL"] = _iconfiguration.GetSection("ProjectVariables").GetSection("apiURL").Value;
             return View();
         }
     }
