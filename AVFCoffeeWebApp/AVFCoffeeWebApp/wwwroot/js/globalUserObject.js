ad3fa5f63521df31e316c69234912280511be47d
mb992 <mb992@cornell.edu>
2017-12-12 09:28:07 -0500

    Simulation page, load with users current numbers

--- AVFCoffeeWebApp/AVFCoffeeWebApp/wwwroot/js/globalUserObject.js
+++ AVFCoffeeWebApp/AVFCoffeeWebApp/wwwroot/js/globalUserObject.js
@@ -13,19 +13,38 @@
             if (status != 'nocontent') {
                 UserData.input = content.loginfo.Inputs;
                 UserData.output = content.loginfo.Outputs;
-                //UserData.userData = content.loginfo.User
+                //find out what page is requesting the info
+                var path = window.location.pathname;
+                var page = path.split("/").pop();
+                console.log(page);
 
-                //load users data into input page
-                $("#earlyHectares").val(UserData.input.earlyHectares);
-                $("#peakHectares").val(UserData.input.peakHectares);
-                $("#oldHectares").val(UserData.input.oldHectares);
-                $("#conventional").prop("checked", UserData.input.conventional);
-                $("#organic").prop("checked", UserData.input.organic);
-                $("#transition").prop("checked", UserData.input.transition);
-                $("#workerSalarySoles").val(UserData.input.workerSalarySoles);
-                $("#productionQuintales").val(UserData.input.productionQuintales);
-                $("#transportCostSoles").val(UserData.input.transportCostSoles);
-                $("#costPriceSolesPerQuintal").val(UserData.input.costPriceSolesPerQuintal);
+                if (page == "Simulation") {
+                    //load users data into simulation page
+                    $("#earlyHectares2").val(UserData.input.earlyHectares);
+                    $("#peakHectares2").val(UserData.input.peakHectares);
+                    $("#oldHectares2").val(UserData.input.oldHectares);
+                    $("#conventional2").prop("checked", UserData.input.conventional);
+                    $("#organic2").prop("checked", UserData.input.organic);
+                    $("#transition2").prop("checked", UserData.input.transition);
+                    $("#workerSalarySoles2").val(UserData.input.workerSalarySoles);
+                    $("#productionQuintales2").val(UserData.input.productionQuintales);
+                    $("#transportCostSoles2").val(UserData.input.transportCostSoles);
+                    $("#costPriceSolesPerQuintal2").val(UserData.input.costPriceSolesPerQuintal);
+                }
+                else if (page == "input") {
+                    //load users data into input page
+                    $("#earlyHectares").val(UserData.input.earlyHectares);
+                    $("#peakHectares").val(UserData.input.peakHectares);
+                    $("#oldHectares").val(UserData.input.oldHectares);
+                    $("#conventional").prop("checked", UserData.input.conventional);
+                    $("#organic").prop("checked", UserData.input.organic);
+                    $("#transition").prop("checked", UserData.input.transition);
+                    $("#workerSalarySoles").val(UserData.input.workerSalarySoles);
+                    $("#productionQuintales").val(UserData.input.productionQuintales);
+                    $("#transportCostSoles").val(UserData.input.transportCostSoles);
+                    $("#costPriceSolesPerQuintal").val(UserData.input.costPriceSolesPerQuintal);
+                }
+               
             }
         },
         error: function () {
