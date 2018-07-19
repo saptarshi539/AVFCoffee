aab48b3e84cdf969b0e82d948353b337812521bc
Rishi <sm954@cornell.edu>
2018-07-19 19:26:46 -0400

    new pages

---
+++ AVFCoffeeWebApp/AVFCoffeeWebApp/wwwroot/pages/TechnicianHome/js/technician.js
@@ -0,0 +1,10 @@
+﻿function change() {
+    debugger;
+    
+    $("#Metrics").removeClass("mdl-navigation__link")
+    $("#Metrics").addClass("mdl-navigation__link is-active");
+}
+
+function press() {
+    debugger;
+}
